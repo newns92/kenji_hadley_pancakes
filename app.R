a44e825e7ba6d2296c732a6ad2edfe2df21cd397
Steve Newns <s.newns92@gmail.com>
2018-02-04 12:19:02 -0500

    start refactoring of hadley's eggnog scaling app into kenji's pancakes

---
+++ app.R
@@ -0,0 +1,88 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+source("./ingredients.r")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+ 
+  # Application title
+  titlePanel("The Best Light and Fluffy Buttermilk Pancakes"),
+  
+  fluidRow(
+    column(width = 4, 
+           p(
+             a("Recipe ",
+               href = "https://http://www.seriouseats.com/recipes/2010/06/light-and-fluffy-pancakes-recipe.html"), 
+             "from ", 
+             a("J. Kenji López-Alt", href = "https://twitter.com/kenjilopezalt"), 
+             "."
+            ),
+           p("Application inspired by",
+             a("eggnogr", href = "https://hadley.shinyapps.io/eggnogr/"), 
+             ", developed ", 
+             a("Hadley Wickham", href = "https://twitter.com/hadleywickham"), 
+             ", with the help of ",
+             a("R", href = "https://www.r-project.org/"),
+             "and ",
+             a("Shiny", href = "http://shiny.rstudio.com"), "."),
+           
+           p("How servings?"),
+           numericInput("quantity", "", 1, min = 1),
+           p("(Makes  6-inch pancakes)"),
+           selectInput("units", "", names(units)),
+           
+           checkboxInput("variation", "Clyde common variation? (no rum)"),
+           checkboxInput("nice", "Nice numbers? (makes vol approx)", TRUE),
+           checkboxInput("metric", "Would you like metric units?")
+    ),
+    column(width = 5,
+           h2("Ingredients"),
+           tableOutput("ingredients"),  
+           p("(all units by volume, not weight)"),
+           h2("Instructions"),
+           tags$ol(
+             tags$li("Beat eggs in blender for one minute on medium speed."),
+             tags$li("Slowly add sugar and blend for one additional minute."),
+             tags$li("With blender still running, add nutmeg, brandy, rum, milk and cream until combined."),
+             tags$li("Chill thoroughly to allow flavors to combine."),
+             tags$li("Serve in chilled wine glasses or champagne coupes, grating additional ",
+                     "nutmeg on top immediately before serving.")
+           )
+    )
+  ),
+  p(a("Read the source", href = "https://github.com/hadley/eggnogr"))
+  
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+  ingredients <- reactive(
+    if (input$dry) dry else dry      
+  )
+  
+  scaled <- reactive({
+    df <- scale(ingredients(), input$quantity, grams = input$grams,
+                ml = input$ml)
+    df$quantity <- format(df$quantity, drop0trailing = TRUE)
+    df
+  })
+  
+  output$ingredients <- renderTable(scaled(), 
+                                    align = "rrll",
+                                    include.rownames = FALSE,
+                                    include.colnames = FALSE)
+   
+}
+
+# Run the application (ALWAYS LAST LINE IN FILE)
+shinyApp(ui = ui, server = server)
+
---
+++ ingredients.r
@@ -0,0 +1,78 @@
+dry <- data.frame(
+  quantity = c(10, 1, 0.5, 1, 1),
+  unit = c("oz", "tsp", "tsp", "tsp", "tbsp"),
+  ingredient = c("All-Purpose Flour", "Baking Powder", "Baking Soda",
+                 "Kosher Salt", "Sugar"),
+  stringsAsFactors = FALSE
+)
+
+#head(dry)
+
+wet <- data.frame(
+  quantity = c(2, 12, 8, 4, NA),
+  unit = c("", "fl. oz", "oz", "tbsp", ""),
+  ingredient = c("Large Eggs (Yolks seperated)", "Buttermilk", "Sour Cream",
+                 "Unsalted Butter (with some more for serving)", "Warm Maple Syrup"),
+  stringsAsFactors = FALSE
+)
+
+#head(wet)
+
+yogurt <- data.frame(
+  quantity = c(2, 8, 1.75, 8, 4, NA),
+  unit = c("", "oz", "fl. oz", "oz", "tbsp", ""),
+  ingredient = c("Large Eggs (Yolks seperated)", "Yogurt", "Milk", "Sour Cream",
+                 "Unsalted Butter (with some more for serving)", "Warm Maple Syrup"),
+  stringsAsFactors = FALSE
+)
+
+sourCream <- data.frame(
+  quantity = c(2, 4, 12, 4, NA),
+  unit = c("", "fl. oz", "oz", "tbsp", ""),
+  ingredient = c("Large Eggs (Yolks seperated)", "Milk", "Sour Cream",
+                 "Unsalted Butter (with some more for serving)", "Warm Maple Syrup"),
+  stringsAsFactors = FALSE
+)
+
+cremeFraiche <- data.frame(
+  quantity = c(2, 4, 4, 12, 4, NA),
+  unit = c("", "fl. oz", "oz", "oz", "tbsp", ""),
+  ingredient = c("Large Eggs (Yolks seperated)", "Milk", "Crème Fraîche", "Sour Cream",
+                 "Unsalted Butter (with some more for serving)", "Warm Maple Syrup"),
+  stringsAsFactors = FALSE
+)
+
+scale <- function(ingredients, quantity = 1, grams = T, ml = F) {
+  
+  cakes <- quantity * 4
+  
+  if (grams) {
+    in_oz <- ingredients$unit == "oz"
+    ingredients$quantity[in_oz] <- round(ingredients$quantity[in_oz] * 28.3495231)
+    ingredients$unit[in_oz] <- "grams"
+    if (ml) {
+      in_floz <- ingredients$unit == "fl. oz"
+      ingredients$quantity[in_floz] <- round(ingredients$quantity[in_floz] * 29.5735296)
+      ingredients$unit[in_oz] <- "mL"    
+    }
+    #return 
+    ingredients
+  }
+  
+# units <- c(
+#     "serving" = 10,
+#     "oz" = 1,
+#     "cup" = 8,
+#     "quart" = 32,
+#     "gallon" = 128
+#   )
+  
+  #to_oz <- function(unit) {
+  #  units[[unit]]
+  #}
+  #from_oz <- function(unit) {
+  #  1 / units[[unit]]
+#}
+ # print(ingredients)
+ # print(cakes)
+}
